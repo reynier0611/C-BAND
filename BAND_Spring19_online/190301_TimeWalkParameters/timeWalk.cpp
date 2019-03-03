1284eace4b68f0ceae4406e936a08b56047142d6
Reynier Cruz Torres <reynier@mit.edu>
2019-03-03 16:26:26 -0500

    Added phase corrections to time-walk code

--- BAND_Spring19_online/190301_TimeWalkParameters/timeWalk.cpp
+++ BAND_Spring19_online/190301_TimeWalkParameters/timeWalk.cpp
@@ -28,6 +28,7 @@ using namespace std;
 
 // Forward-declaring functions
 void PrettyTH2F(TH2F * h2);
+double getTriggerPhase( long timeStamp );
 
 int slc[6][5] = {{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,5,5,0},{3,7,6,6,2}};
 // ========================================================================================================================================
@@ -62,17 +63,17 @@ int main(int argc, char** argv) {
 	TH2F ** h2_tdc_adc_L = new TH2F * [nHistos];
 	TH2F ** h2_tdc_adc_R = new TH2F * [nHistos];
 
-	TH2F * h2_tdc_adc_tot_L = new TH2F("h2_tdc_adc_tot_L","Left PMTs;ADC;t_{TDC}-t_{FADC} [ns]" ,200,0,20000,200,350,450);
-	TH2F * h2_tdc_adc_tot_R = new TH2F("h2_tdc_adc_tot_R","Right PMTs;ADC;t_{TDC}-t_{FADC} [ns]",200,0,20000,200,350,450);
+	TH2F * h2_tdc_adc_tot_L = new TH2F("h2_tdc_adc_tot_L","Left PMTs;ADC;t_{TDC}-t_{FADC} [ns]" ,400,1000,14000,400,370,400);
+	TH2F * h2_tdc_adc_tot_R = new TH2F("h2_tdc_adc_tot_R","Right PMTs;ADC;t_{TDC}-t_{FADC} [ns]",400,1000,14000,400,370,400);
 	h2_tdc_adc_tot_L -> GetYaxis() -> SetTitleOffset(1.40);
-        h2_tdc_adc_tot_L -> GetXaxis() -> SetNdivisions(509);
+	h2_tdc_adc_tot_L -> GetXaxis() -> SetNdivisions(509);
 	h2_tdc_adc_tot_R -> GetYaxis() -> SetTitleOffset(1.40);
-        h2_tdc_adc_tot_R -> GetXaxis() -> SetNdivisions(509);
+	h2_tdc_adc_tot_R -> GetXaxis() -> SetNdivisions(509);
 
 	for(int i = 0 ; i < nHistos ; i++){
-		h2_tdc_adc_L[i] = new TH2F(Form("h2_tdc_adc_L_%i",i),";ADC;t_{TDC}-t_{FADC} [ns]",200,0,20000,200,350,450);
-		h2_tdc_adc_R[i] = new TH2F(Form("h2_tdc_adc_R_%i",i),";ADC;t_{TDC}-t_{FADC} [ns]",200,0,20000,200,350,450);
-	
+		h2_tdc_adc_L[i] = new TH2F(Form("h2_tdc_adc_L_%i",i),";ADC;t_{TDC}-t_{FADC} [ns]",400,1000,14000,400,370,400);
+		h2_tdc_adc_R[i] = new TH2F(Form("h2_tdc_adc_R_%i",i),";ADC;t_{TDC}-t_{FADC} [ns]",400,1000,14000,400,370,400);
+
 		PrettyTH2F(h2_tdc_adc_L[i]);
 		PrettyTH2F(h2_tdc_adc_R[i]);
 	}
@@ -82,8 +83,9 @@ int main(int argc, char** argv) {
 	hipo::reader reader;
 	reader.open(inputFile);
 
-	hipo::bank BAND_ADC("BAND::adc",reader);
-	hipo::bank BAND_TDC("BAND::tdc",reader);
+	hipo::bank BAND_ADC  ("BAND::adc"  ,reader);
+	hipo::bank BAND_TDC  ("BAND::tdc"  ,reader);
+	hipo::bank RUN_config("RUN::config",reader);
 
 	int event_counter = 0;
 	// ----------------------------------------------------------------------------------
@@ -95,12 +97,19 @@ int main(int argc, char** argv) {
 
 		//BAND_ADC.show();
 		//BAND_TDC.show();
+		//RUN_config.show();
 
 		int nADC = BAND_ADC.getSize();
 		int nTDC = BAND_TDC.getSize();
 
 		// Skip events with no entries
 		if(nADC==0||nTDC==0) continue;
+
+		// Skip events with less than 200 entries (the laser lights all bars at the same time)
+		if(nADC<200||nTDC<200) continue;
+
+		long timestamp = RUN_config.getLong(4,0);
+		double phaseCorr = getTriggerPhase(timestamp);
 
 		for(int aIdx = 0 ; aIdx < nADC ; aIdx++){
 
@@ -126,18 +135,20 @@ int main(int argc, char** argv) {
 				  ){
 					int barKey = 100*ADC_sector + 10*ADC_layer + ADC_component;
 
+					double deltaT_phaseCorr = (TDC_time - phaseCorr) - ADC_time;
+
 					// Left PMTs
 					if(ADC_order==0){
 						h2_tdc_adc_L[barKey] -> SetTitle(Form("Left PMTs, Sector:%i, Layer:%i, Component:%i",ADC_sector,ADC_layer,ADC_component));
-						h2_tdc_adc_L[barKey] -> Fill(ADC_adc,TDC_time-ADC_time);
-						h2_tdc_adc_tot_L     -> Fill(ADC_adc,TDC_time-ADC_time);
+						h2_tdc_adc_L[barKey] -> Fill(ADC_adc,deltaT_phaseCorr);
+						h2_tdc_adc_tot_L     -> Fill(ADC_adc,deltaT_phaseCorr);
 					}
 
 					// Right PMTs
 					if(ADC_order==1){
 						h2_tdc_adc_R[barKey] -> SetTitle(Form("Right PMTs, Sector:%i, Layer:%i, Component:%i",ADC_sector,ADC_layer,ADC_component));
-						h2_tdc_adc_R[barKey] -> Fill(ADC_adc,TDC_time-ADC_time);
-						h2_tdc_adc_tot_R     -> Fill(ADC_adc,TDC_time-ADC_time);
+						h2_tdc_adc_R[barKey] -> Fill(ADC_adc,deltaT_phaseCorr);
+						h2_tdc_adc_tot_R     -> Fill(ADC_adc,deltaT_phaseCorr);
 					}
 
 				}
@@ -194,7 +205,7 @@ int main(int argc, char** argv) {
 					cSLC[is][il] -> cd(2*cIdx+1);
 					gPad -> SetBottomMargin(0.26);
 					h2_tdc_adc_L[identifier] -> Draw("COLZ");
-					
+
 					cSLC[is][il] -> cd(2*cIdx+2);
 					gPad -> SetBottomMargin(0.26);
 					h2_tdc_adc_R[identifier] -> Draw("COLZ");
@@ -213,21 +224,21 @@ int main(int argc, char** argv) {
 		for(int il = 1 ; il <= 6 ; il++){
 			for(int ic = 1 ; ic <= slc[il-1][is-1] ; ic++){
 				int idx = 100*is + 10*il + ic;
-				
+
 				if(parL[idx][0]!=0){
 					tabL << is << "\t" << il << "\t" << ic << "\t" << "\t";
 					tabL << parL[idx][0] << "\t" << parL[idx][1] << "\t" << parL[idx][2]  << "\t" << parL[idx][3] << endl;
 				}
 				// ---
 				if(parR[idx][0]!=0){
-                                        tabR << is << "\t" << il << "\t" << ic << "\t" << "\t";
-                                        tabR << parR[idx][0] << "\t" << parR[idx][1] << "\t" << parR[idx][2]  << "\t" << parR[idx][3] << endl;
-                                }
+					tabR << is << "\t" << il << "\t" << ic << "\t" << "\t";
+					tabR << parR[idx][0] << "\t" << parR[idx][1] << "\t" << parR[idx][2]  << "\t" << parR[idx][3] << endl;
+				}
 			}
 		}
 	}
 	tabL.close();
-        tabR.close();
+	tabR.close();
 
 	// -------------------------------------------------------------------------------------------------
 	// Deleting empty histograms
@@ -240,13 +251,13 @@ int main(int argc, char** argv) {
 	}
 
 	// -------------------------------------------------------------------------------------------------
-        // Saving plots to a pdf file
+	// Saving plots to a pdf file
 	c0 -> Print("results_timeWalk.pdf(");
 	for(int is = 0 ; is < 5 ; is++){
-                for(int il = 0 ; il < 6 ; il++){
-                        cSLC[is][il] -> Print("results_timeWalk.pdf");
-                }
-        }
+		for(int il = 0 ; il < 6 ; il++){
+			cSLC[is][il] -> Print("results_timeWalk.pdf");
+		}
+	}
 	c0 -> Print("results_timeWalk.pdf)");
 
 	myapp -> Run();
@@ -262,11 +273,23 @@ void PrettyTH2F(TH2F * h2) {
 	h2 -> GetXaxis() -> SetLabelSize(0.13);
 	h2 -> GetYaxis() -> SetLabelSize(0.13);
 	h2 -> GetXaxis() -> SetTitleSize(0.13);
-        h2 -> GetYaxis() -> SetTitleSize(0.13);
-	
+	h2 -> GetYaxis() -> SetTitleSize(0.13);
+
 	h2 -> GetYaxis() -> SetTitleOffset(0.38);
 	h2 -> GetYaxis() -> SetNdivisions(509);
 
 	h2 -> GetXaxis() -> SetNdivisions(509);
 }
+// ========================================================================================================================================
+double getTriggerPhase( long timeStamp ) {
+	double tPh = 0.;
 
+	long Period = 4.0;
+	long Cycles = 6.0;
+	long Phase  = 3.0;
+
+	if( timeStamp != -1 ) 
+		tPh = (double)(Period *( (timeStamp + Phase) % Cycles ));
+
+	return tPh;
+}
