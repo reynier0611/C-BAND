aff0dec610853bd90acf5531fd4e745f913d608e
Reynier Cruz Torres <reynier@mit.edu>
2019-03-26 17:29:11 -0400

    Updated code and added option to select ranges of ADC_L values to cut

--- BAND_Spring19_online/190314_Attenuation/diff_adc_tdc.cpp
+++ BAND_Spring19_online/190314_Attenuation/diff_adc_tdc.cpp
@@ -55,13 +55,25 @@ int main(int argc, char** argv) {
 
 	TString inputFile;
 
-	if(argc==2) {
+	if(argc==3) {
 		inputFile = argv[1];
 	}
 	else {
-		cout << "=========================\nRun this code as:\n./code path/to/input/file\n=========================" << endl;
+		cout << "=========================\nRun this code as:\n./code path/to/input/file A" << endl;
+		cout << "where A = 0 -> Altogether"             << endl;
+		cout << "        = 1 -> ADC_L < 11000"          << endl;
+		cout << "        = 2 -> 11000 <= ADC_L < 18000" << endl;
+		cout << "        = 3 -> ADC_L >= 18000"         << endl;
+		cout << "\n========================="           << endl;
 		exit(0);
 	}
+
+	int cutopt = atoi(argv[2]);
+	TLatex * tex_cut = new TLatex();
+	if(cutopt==0) tex_cut = new TLatex(0.1,0.5,"Altogether"            );
+	if(cutopt==1) tex_cut = new TLatex(0.1,0.5,"ADC_L < 11000"         );
+	if(cutopt==2) tex_cut = new TLatex(0.1,0.5,"11000 <= ADC_L < 18000");
+	if(cutopt==3) tex_cut = new TLatex(0.1,0.5,"ADC_L >= 18000"        );
 
 	// ----------------------------------------------------------------------------------
 	// Declaring histograms
@@ -70,10 +82,10 @@ int main(int argc, char** argv) {
 	TH2F ** h2_diff_adc_tdc = new TH2F * [nHistos];
 
 	for(int i = 0 ; i < nHistos ; i++){
-		h2_diff_adc_tdc[i] = new TH2F(Form("h2_diff_adc_tdc_%i",i),";TDC L-R;ADC L-R",100,-20,20,100,-50,50);
+		h2_diff_adc_tdc[i] = new TH2F(Form("h2_diff_adc_tdc_%i",i),";TDC L-R;ADC L-R",200,-20,20,200,-50,50);
 		PrettyTH2F(h2_diff_adc_tdc[i]);
 	}
-	
+
 	// ----------------------------------------------------------------------------------
 	// Opening input HIPO file
 	hipo::reader reader;
@@ -86,7 +98,7 @@ int main(int argc, char** argv) {
 	int event_counter = 0;
 	// ----------------------------------------------------------------------------------
 	// Loop over events and print them on the screen
-	
+
 	while(reader.next()==true){
 
 		if(event_counter%1000==0) cout << "event: " << event_counter << endl;
@@ -98,7 +110,7 @@ int main(int argc, char** argv) {
 
 		int nADC = BAND_ADC.getSize();
 		int nTDC = BAND_TDC.getSize();
-		
+
 		// Skip events with no entries
 		if(nADC==0||nTDC==0) continue;
 
@@ -107,11 +119,11 @@ int main(int argc, char** argv) {
 
 		// Skip all laser events
 		if(nADC>100||nTDC>100) continue;
-		
+
 		double phaseCorr = 0;
 		//long timestamp = RUN_config.getLong(4,0);
 		//double phaseCorr = getTriggerPhase(timestamp);
-		
+
 		for(int aIdx1 = 0 ; aIdx1 < nADC ; aIdx1++){
 
 			int   ADC1_sector    = BAND_ADC.getInt  (0,aIdx1);
@@ -168,22 +180,28 @@ int main(int argc, char** argv) {
 								TDC2_time -= phaseCorr;
 
 								if(             (!already_matched                     )&& // Avoid multi-hits
-									        (TDC1_sector   ==TDC2_sector          )&&
+										(TDC1_sector   ==TDC2_sector          )&&
 										(TDC1_layer    ==TDC2_layer           )&&
 										(TDC1_component==TDC2_component       )&&
 										(TDC1_order+1==TDC2_order             )
 								  ){	
-							
+
 									already_matched = true;
 
 									int barKey = 100*ADC1_sector + 10*ADC1_layer + ADC1_component;			
 
 									double diff_ADC = ADC1_time - ADC2_time;
 									double diff_TDC = TDC1_time - TDC2_time;
-									
-									h2_diff_adc_tdc[barKey] -> Fill( diff_TDC , diff_ADC );
+
+									if(             ( cutopt == 0)||
+											((cutopt == 1)&&(ADC1_adc <  11000))||
+											((cutopt == 2)&&(ADC1_adc >= 11000)&&(ADC1_adc < 18000))||
+											((cutopt == 3)&&(ADC1_adc >= 18000))
+									  ){
+										h2_diff_adc_tdc[barKey] -> Fill( diff_TDC , diff_ADC );
+									}
 									h2_diff_adc_tdc[barKey] -> SetTitle(Form("Sector: %i, Layer: %i, Component: %i",TDC1_sector,TDC1_layer,TDC1_component));
-									
+
 								}
 							}
 						}
@@ -191,18 +209,21 @@ int main(int argc, char** argv) {
 				}
 			}
 		}
-		
+
 	}// end file
-	
+
 	// -------------------------------------------------------------------------------------------------
 	// Printing results onto canvases
 	TCanvas * c0 = new TCanvas("c0","c0",900,900);
+	tex_cut -> Draw("same");
+	c0 -> Modified();
+	c0 -> Update();
 
 	TCanvas *** cSLC_paddle = new TCanvas**[5];
 
 	for(int is = 0 ; is < 5 ; is++){
 		cSLC_paddle[is] = new TCanvas*[6];
-		
+
 
 		for(int il = 0 ; il < 5 ; il++){
 			cSLC_paddle[is][il] = new TCanvas(Form("S%iL%i_paddle",is,il),Form("Sector %i, Layer %i",is+1,il+1),900,900);
@@ -216,71 +237,21 @@ int main(int argc, char** argv) {
 					cSLC_paddle[is][il] -> cd(cIdx+1);
 					gPad -> SetBottomMargin(0.26);
 					gPad -> SetLeftMargin(0.14);
-					//min = getMinNonEmptyBin(h2_diff_adc_tdc[identifier]);
-					//max = getMaxNonEmptyBin(h2_diff_adc_tdc[identifier]);
-					//h2_diff_adc_tdc[identifier] -> GetYaxis() -> SetRange(min,max);
+					min = getMinNonEmptyBin(h2_diff_adc_tdc[identifier]);
+					max = getMaxNonEmptyBin(h2_diff_adc_tdc[identifier]);
+					h2_diff_adc_tdc[identifier] -> GetYaxis() -> SetRange(min,max);
 					h2_diff_adc_tdc[identifier] -> Draw("COLZ");
 
 					/*
-					TLatex * tex_pad = new TLatex(1000,par_pad[identifier][0]-5,Form("y = %f #pm %f ns",par_pad[identifier][0],par_pad[identifier][1]));
-					tex_pad -> SetTextSize(0.08);
-					tex_pad -> Draw("same");
-					*/
+					   TLatex * tex_pad = new TLatex(1000,par_pad[identifier][0]-5,Form("y = %f #pm %f ns",par_pad[identifier][0],par_pad[identifier][1]));
+					   tex_pad -> SetTextSize(0.08);
+					   tex_pad -> Draw("same");
+					 */
 				} 
 			}
 			cSLC_paddle[is][il] -> Modified();	cSLC_paddle[is][il] -> Update();		
 		}
 	}
-	/*
-	// -------------------------------------------------------------------------------------------------
-	// Making a plot with resolutions
-	int ctr = 0;
-	double x_val[600] = {0};
-	double y_val[600] = {0};
-	for(int il = 1 ; il < 6 ; il++){
-		for(int is = 1 ; is <= 5 ; is++){
-			for(int ic = 1 ; ic <= slc[il-1][is-1] ; ic++){
-				int I_BarKey = 100*is + 10*il + ic;
-
-				x_val[ctr] = (double)(I_BarKey);
-				y_val[ctr] = 1000*par_res[I_BarKey][0]/TMath::Sqrt(2);
-				ctr++;	
-			}
-		}
-	}
-
-	TGraphErrors * gResolutions = new TGraphErrors(ctr,x_val,y_val,0,0);
-	PrettyTGraphErrors(gResolutions,62);
-
-	TCanvas * cres = new TCanvas("cres","cres",900,900);
-	gResolutions -> Draw("AP");
-	cres -> Modified();
-	cres -> Update();
-
-	// -------------------------------------------------------------------------------------------------
-	// Saving fit values to ccdb tables
-	ofstream tab_pad, tab_lay;
-	tab_pad.open("TDC_paddle_offsets.txt");
-	tab_lay.open("TDC_layer_offsets.txt" );
-
-	for(int il = 1 ; il <= 6 ; il++){
-		for(int is = 1 ; is <= 5 ; is++){
-			for(int ic = 1 ; ic <= slc[il-1][is-1] ; ic++){
-				int idx = 100*is + 10*il + ic;
-
-				tab_pad << is << "\t" << il << "\t" << ic << "\t";
-				if(il==6||(is==2&&ic==1)) tab_pad << "0.0000\t0.0000" << endl;
-				else tab_pad << par_pad[idx][0] << "\t" << par_pad[idx][1] << endl;
-
-				tab_lay << is << "\t" << il << "\t" << ic << "\t";
-				if(il==6||il==5) tab_lay << "0.0000\t0.0000" << endl;
-				else tab_lay << par_lay[il-1][0] << "\t" << par_lay[il-1][1] << endl;
-			}
-		}
-	}
-	tab_pad.close();
-	tab_lay.close();
-
 	// -------------------------------------------------------------------------------------------------
 	// Deleting empty histograms
 	for(int i = 0 ; i < nHistos ; i++){
@@ -288,29 +259,19 @@ int main(int argc, char** argv) {
 		if(!notEmpty){
 			delete h2_diff_adc_tdc[i];
 		}
-
-		notEmpty = (h2_dMeantime_adc_layer[i]->Integral());
-		if(!notEmpty){
-			delete h2_dMeantime_adc_layer[i];
-		}
 	}
 
 	// -------------------------------------------------------------------------------------------------
 	// Saving plots to a pdf file
-	c0 -> Print("results_paddle_corr.pdf(");
-	cSLC_layer -> Print("results_paddle_corr.pdf");
-	c0 -> Print("results_paddle_corr.pdf");
-	cres -> Print("results_resolutions.pdf(");
+	c0 -> Print(Form("results_diff_adc_tdc_%i.pdf(",cutopt));
 
 	for(int is = 0 ; is < 5 ; is++){
 		for(int il = 0 ; il < 5 ; il++){
-			cSLC_paddle[is][il] -> Print("results_paddle_corr.pdf");
-			cSLC_resol [is][il] -> Print("results_resolutions.pdf");
+			cSLC_paddle[is][il] -> Print(Form("results_diff_adc_tdc_%i.pdf",cutopt));
 		}
 	}
-	c0 -> Print("results_paddle_corr.pdf)");
-	c0 -> Print("results_resolutions.pdf)");
-	*/
+	c0 -> Print(Form("results_diff_adc_tdc_%i.pdf)",cutopt));
+
 	myapp -> Run();
 	return 0;
 }
@@ -402,7 +363,7 @@ void PrettyTGraphErrors(TGraphErrors * gP, int color){
 	gP -> GetXaxis() -> CenterTitle();
 	gP -> GetXaxis() -> SetTitle("Bar ID");
 	gP -> GetYaxis() -> CenterTitle();
-        gP -> GetYaxis() -> SetTitle("resolution/#sqrt{2} [ps]");
+	gP -> GetYaxis() -> SetTitle("resolution/#sqrt{2} [ps]");
 
 	gP -> GetYaxis() -> SetTitleOffset(1.30);
 
