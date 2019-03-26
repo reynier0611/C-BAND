d75c0830e5354f40555e6f5f66b7b643ae8186fb
Reynier Cruz Torres <reynier@mit.edu>
2019-03-26 16:44:53 -0400

    Fixed a couple of things in 'attenuation.cpp' and started writing 'diff_adc_tdc.cpp' to check why attenuation lengths are so short

--- BAND_Spring19_online/190314_Attenuation/diff_adc_tdc.cpp
+++ BAND_Spring19_online/190314_Attenuation/diff_adc_tdc.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "TLine.h"
+#include "TProfile.h"
 #include "TRint.h"
 #include "TApplication.h"
 #include "TFile.h"
@@ -11,11 +13,11 @@
 #include "TMath.h"
 #include "TVector3.h"
 #include "TLorentzVector.h"
+#include "TGraphErrors.h"
 #include "TCanvas.h"
 #include "TStyle.h"
 #include "TLatex.h"
 #include "TLegend.h"
-#include "TProfile.h"
 
 #include "reader.h"
 #include "node.h"
@@ -32,17 +34,13 @@ using namespace std;
 // Forward-declaring functions
 void PrettyTH1F(TH1F * h1, int color);
 void PrettyTH2F(TH2F * h2);
+void PrettyTGraphErrors(TGraphErrors * gP, int color);
 double getTriggerPhase( long timeStamp );
 int getMinNonEmptyBin(TH2F * h2);
 int getMaxNonEmptyBin(TH2F * h2);
-void LoadEffectiveVelocity();
-void LoadEfrainsAttenuationLengths();
 
 int slc[6][5] = {{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,5,5,0},{3,7,6,6,2}};
 
-double mean_c = 14; //cm/ns speed of light in the bar
-double eff_c    [600] = {0};
-double mu_efrain[600] = {0};
 // ========================================================================================================================================
 int main(int argc, char** argv) {
 
@@ -65,29 +63,17 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 
-	LoadEffectiveVelocity        ();
-	LoadEfrainsAttenuationLengths();
-	
 	// ----------------------------------------------------------------------------------
 	// Declaring histograms
 	const int nHistos = 600;
 
-	double par[nHistos][2] = {{0}};
-
-	TH2F ** h2_T_vDt = new TH2F * [nHistos];
-	TH1F ** h1_ADC_L = new TH1F * [nHistos];
-	TH1F ** h1_ADC_R = new TH1F * [nHistos];
+	TH2F ** h2_diff_adc_tdc = new TH2F * [nHistos];
 
 	for(int i = 0 ; i < nHistos ; i++){
-		h2_T_vDt[i] = new TH2F(Form("h2_T_vDt_%i",i),";position [m];ln(R)",200,-1.5,1.5,200,-1,1);
-		PrettyTH2F(h2_T_vDt[i]);
-	
-		h1_ADC_L[i] = new TH1F(Form("h1_ADC_L_%i",i),";ADC",100,0,50000);	PrettyTH1F(h1_ADC_L[i] , 2);
-        	h1_ADC_R[i] = new TH1F(Form("h1_ADC_R_%i",i),";ADC",100,0,50000);	PrettyTH1F(h1_ADC_R[i] ,62);
+		h2_diff_adc_tdc[i] = new TH2F(Form("h2_diff_adc_tdc_%i",i),";TDC L-R;ADC L-R",100,-20,20,100,-50,50);
+		PrettyTH2F(h2_diff_adc_tdc[i]);
 	}
-
-	TH1F * h1_E_minus_R_mu = new TH1F("h1_E_minus_R_mu",";#mu (Efrain - Rey) [m]",40,-2,2); // Attenuation lengths from Efrain - from Rey
-
+	
 	// ----------------------------------------------------------------------------------
 	// Opening input HIPO file
 	hipo::reader reader;
@@ -100,6 +86,7 @@ int main(int argc, char** argv) {
 	int event_counter = 0;
 	// ----------------------------------------------------------------------------------
 	// Loop over events and print them on the screen
+	
 	while(reader.next()==true){
 
 		if(event_counter%1000==0) cout << "event: " << event_counter << endl;
@@ -111,7 +98,7 @@ int main(int argc, char** argv) {
 
 		int nADC = BAND_ADC.getSize();
 		int nTDC = BAND_TDC.getSize();
-
+		
 		// Skip events with no entries
 		if(nADC==0||nTDC==0) continue;
 
@@ -120,13 +107,12 @@ int main(int argc, char** argv) {
 
 		// Skip all laser events
 		if(nADC>100||nTDC>100) continue;
-
-		long timestamp = RUN_config.getLong(4,0);
-		double phaseCorr = getTriggerPhase(timestamp);
-
+		
+		double phaseCorr = 0;
+		//long timestamp = RUN_config.getLong(4,0);
+		//double phaseCorr = getTriggerPhase(timestamp);
+		
 		for(int aIdx1 = 0 ; aIdx1 < nADC ; aIdx1++){
-
-			bool matchedBarADC = false;
 
 			int   ADC1_sector    = BAND_ADC.getInt  (0,aIdx1);
 			int   ADC1_layer     = BAND_ADC.getInt  (1,aIdx1);
@@ -145,17 +131,15 @@ int main(int argc, char** argv) {
 				float ADC2_time      = BAND_ADC.getFloat(5,aIdx2);
 
 				// Matching each ADC to the corresponding ADC from the other side of the bar
-				if(             (!matchedBarADC                       )&&
-						(ADC1_sector   ==ADC2_sector          )&&
+				if(             (ADC1_sector   ==ADC2_sector          )&&
 						(ADC1_layer    ==ADC2_layer           )&&
 						(ADC1_component==ADC2_component       )&&
 						(ADC1_order+1==ADC2_order             )
 				  ){
-					matchedBarADC = true;
 
-					bool matchedBarTDC = false;
-
+					bool already_matched = false;
 					for(int tIdx1 = 0 ; tIdx1 < nTDC ; tIdx1++){
+
 						int   TDC1_sector    = BAND_TDC.getInt  (0,tIdx1);
 						int   TDC1_layer     = BAND_TDC.getInt  (1,tIdx1);
 						int   TDC1_component = BAND_TDC.getInt  (2,tIdx1);
@@ -168,157 +152,165 @@ int main(int argc, char** argv) {
 						// Matching these ADCs to a TDC
 						if(             (ADC1_sector   ==TDC1_sector   )&&
 								(ADC1_layer    ==TDC1_layer    )&&
-								(ADC1_component==TDC1_component)
+								(ADC1_component==TDC1_component)&&
+								(ADC1_order+2  ==TDC1_order    )
 						  ){
 
-							for(int tIdx2 = 0 ; tIdx2 < nADC ; tIdx2++){
+							for(int tIdx2 = 0 ; tIdx2 < nTDC ; tIdx2++){
 
 								int   TDC2_sector    = BAND_TDC.getInt  (0,tIdx2);
 								int   TDC2_layer     = BAND_TDC.getInt  (1,tIdx2);
 								int   TDC2_component = BAND_TDC.getInt  (2,tIdx2);
 								int   TDC2_order     = BAND_TDC.getInt  (3,tIdx2);
 								float TDC2_tdc       = (float)(BAND_TDC.getInt(4,tIdx2));
-								float TDC2_time      = TDC2_tdc*0.02345; 
+								float TDC2_time      = TDC2_tdc*0.02345;
 
 								TDC2_time -= phaseCorr;
 
-								if(             (!matchedBarTDC                       )&&
-										(TDC1_sector   ==TDC2_sector          )&&
+								if(             (!already_matched                     )&& // Avoid multi-hits
+									        (TDC1_sector   ==TDC2_sector          )&&
 										(TDC1_layer    ==TDC2_layer           )&&
 										(TDC1_component==TDC2_component       )&&
 										(TDC1_order+1==TDC2_order             )
-								  ){
-									matchedBarTDC = true;
+								  ){	
+							
+									already_matched = true;
 
-									int barKey = 100*ADC1_sector + 10*ADC1_layer + ADC1_component;
+									int barKey = 100*ADC1_sector + 10*ADC1_layer + ADC1_component;			
 
-									double lnR = TMath::Log( ADC1_adc/ADC2_adc );
-									double deltaT = TDC1_time-TDC2_time;
-									//double x = deltaT*mean_c/100./2.;
-									double x = deltaT*eff_c[barKey]/100./2.;
-
-									h2_T_vDt[barKey] -> Fill(x,lnR);
-									h2_T_vDt[barKey] -> SetTitle(Form("Sector: %i, Layer: %i, Component: %i",TDC1_sector,TDC1_layer,TDC1_component));
-								
-									h1_ADC_L[barKey] -> Fill(ADC1_adc);	h1_ADC_L[barKey] -> SetTitle(Form("Sector: %i, Layer: %i, Component: %i",TDC1_sector,TDC1_layer,TDC1_component));
-									h1_ADC_R[barKey] -> Fill(ADC2_adc);	h1_ADC_R[barKey] -> SetTitle(Form("Sector: %i, Layer: %i, Component: %i",TDC1_sector,TDC1_layer,TDC1_component));
-								}			
+									double diff_ADC = ADC1_time - ADC2_time;
+									double diff_TDC = TDC1_time - TDC2_time;
+									
+									h2_diff_adc_tdc[barKey] -> Fill( diff_TDC , diff_ADC );
+									h2_diff_adc_tdc[barKey] -> SetTitle(Form("Sector: %i, Layer: %i, Component: %i",TDC1_sector,TDC1_layer,TDC1_component));
+									
+								}
 							}
 						}
-					}		
+					}
 				}
-			}		
+			}
 		}
-
-	}// end file
-
-	// -------------------------------------------------------------------------------------------------
-	// Fitting functions
-	TProfile ** p_T_vDt = new TProfile*[nHistos];
-	for(int i = 0 ; i < nHistos ; i++){
-		int notEmpty = (h2_T_vDt[i]->Integral());
-		if(notEmpty){
-			int nBins = h2_T_vDt[i] -> GetYaxis() -> GetNbins();
-                        p_T_vDt[i] = h2_T_vDt[i]->ProfileX(Form("px_%i",i), 1, nBins );
-
-			TF1 * f_atten = new TF1("f_atten","pol1",-1,1);
-
-			p_T_vDt[i] -> Fit("f_atten","QR");
-
-			par[i][0] = -2/(f_atten->GetParameter(1));		//slope
-			par[i][1] = 2*(f_atten->GetParError(1))/pow(par[i][0],2);	//error
 		
-			p_T_vDt[i] -> SetMarkerStyle(20);
-                        p_T_vDt[i] -> SetMarkerColor( 1);
-                        p_T_vDt[i] -> SetMarkerSize (.5);
-		}
-	}
-
+	}// end file
+	
 	// -------------------------------------------------------------------------------------------------
 	// Printing results onto canvases
 	TCanvas * c0 = new TCanvas("c0","c0",900,900);
 
-	TCanvas *** cSLC = new TCanvas**[5];
-	TCanvas *** cADC = new TCanvas**[5];
-
-	TLegend * leg_adc = new TLegend(0.6,0.6,0.85,0.75);
-	leg_adc -> SetLineColor(0);
-	leg_adc -> AddEntry(h1_ADC_L[0],"Left PMT" );
-	leg_adc -> AddEntry(h1_ADC_R[0],"Right PMT");
+	TCanvas *** cSLC_paddle = new TCanvas**[5];
 
 	for(int is = 0 ; is < 5 ; is++){
-		cSLC[is] = new TCanvas*[6];
-		cADC[is] = new TCanvas*[6];
-		for(int il = 0 ; il < 5 ; il++){
-			cSLC[is][il] = new TCanvas(Form("S%iL%i",is,il),Form("Sector %i, Layer %i",is+1,il+1),900,900);
-			cSLC[is][il] -> Divide(2,4);
+		cSLC_paddle[is] = new TCanvas*[6];
+		
 
-			cADC[is][il] = new TCanvas(Form("cADC_S%iL%i",is,il),Form("Sector %i, Layer %i",is+1,il+1),900,900);
-			cADC[is][il] -> Divide(2,4);
+		for(int il = 0 ; il < 5 ; il++){
+			cSLC_paddle[is][il] = new TCanvas(Form("S%iL%i_paddle",is,il),Form("Sector %i, Layer %i",is+1,il+1),900,900);
+			cSLC_paddle[is][il] -> Divide(2,4);			
 
 			for(int cIdx = 0 ; cIdx < slc[il][is] ; cIdx++){
 				int identifier = 100*(is+1)+10*(il+1)+(cIdx+1);
-				int notEmpty = (h2_T_vDt[identifier]->Integral());
+				int notEmpty = (h2_diff_adc_tdc[identifier]->Integral());
 				if(notEmpty){
 					int min, max;
-					cSLC[is][il] -> cd(cIdx+1);
+					cSLC_paddle[is][il] -> cd(cIdx+1);
 					gPad -> SetBottomMargin(0.26);
 					gPad -> SetLeftMargin(0.14);
-					min = getMinNonEmptyBin(h2_T_vDt[identifier]);
-					max = getMaxNonEmptyBin(h2_T_vDt[identifier]);
-					h2_T_vDt[identifier] -> GetYaxis() -> SetRange(min,max);
-					h2_T_vDt[identifier] -> Draw("COLZ");
+					//min = getMinNonEmptyBin(h2_diff_adc_tdc[identifier]);
+					//max = getMaxNonEmptyBin(h2_diff_adc_tdc[identifier]);
+					//h2_diff_adc_tdc[identifier] -> GetYaxis() -> SetRange(min,max);
+					h2_diff_adc_tdc[identifier] -> Draw("COLZ");
 
-					p_T_vDt[identifier] -> Draw("same");
-
-					TLatex * tex_mu = new TLatex(-1.4,-0.1,Form("#mu = %.3f #pm %.4f m",par[identifier][0],par[identifier][1]));
-					tex_mu -> SetTextSize(0.08);
-					tex_mu -> Draw("same");
-
-					h1_E_minus_R_mu -> Fill(mu_efrain[identifier]/100.-par[identifier][0]);
-				
-					// ADC spectra
-					cADC[is][il] -> cd(cIdx+1);
-					gPad -> SetBottomMargin(0.26);
-
-					h1_ADC_L[identifier] -> Draw(); 
-					h1_ADC_R[identifier] -> Draw("same");
-					leg_adc -> Draw("same");
-				}
+					/*
+					TLatex * tex_pad = new TLatex(1000,par_pad[identifier][0]-5,Form("y = %f #pm %f ns",par_pad[identifier][0],par_pad[identifier][1]));
+					tex_pad -> SetTextSize(0.08);
+					tex_pad -> Draw("same");
+					*/
+				} 
 			}
-			cSLC[is][il] -> Modified();	cSLC[is][il] -> Update();
-			cADC[is][il] -> Modified();	cADC[is][il] -> Update();
+			cSLC_paddle[is][il] -> Modified();	cSLC_paddle[is][il] -> Update();		
+		}
+	}
+	/*
+	// -------------------------------------------------------------------------------------------------
+	// Making a plot with resolutions
+	int ctr = 0;
+	double x_val[600] = {0};
+	double y_val[600] = {0};
+	for(int il = 1 ; il < 6 ; il++){
+		for(int is = 1 ; is <= 5 ; is++){
+			for(int ic = 1 ; ic <= slc[il-1][is-1] ; ic++){
+				int I_BarKey = 100*is + 10*il + ic;
+
+				x_val[ctr] = (double)(I_BarKey);
+				y_val[ctr] = 1000*par_res[I_BarKey][0]/TMath::Sqrt(2);
+				ctr++;	
+			}
 		}
 	}
 
-	TCanvas * c2 = new TCanvas("c2");
-	h1_E_minus_R_mu -> Draw();
-	c2 -> Modified();
-	c2 -> Update();
+	TGraphErrors * gResolutions = new TGraphErrors(ctr,x_val,y_val,0,0);
+	PrettyTGraphErrors(gResolutions,62);
+
+	TCanvas * cres = new TCanvas("cres","cres",900,900);
+	gResolutions -> Draw("AP");
+	cres -> Modified();
+	cres -> Update();
+
+	// -------------------------------------------------------------------------------------------------
+	// Saving fit values to ccdb tables
+	ofstream tab_pad, tab_lay;
+	tab_pad.open("TDC_paddle_offsets.txt");
+	tab_lay.open("TDC_layer_offsets.txt" );
+
+	for(int il = 1 ; il <= 6 ; il++){
+		for(int is = 1 ; is <= 5 ; is++){
+			for(int ic = 1 ; ic <= slc[il-1][is-1] ; ic++){
+				int idx = 100*is + 10*il + ic;
+
+				tab_pad << is << "\t" << il << "\t" << ic << "\t";
+				if(il==6||(is==2&&ic==1)) tab_pad << "0.0000\t0.0000" << endl;
+				else tab_pad << par_pad[idx][0] << "\t" << par_pad[idx][1] << endl;
+
+				tab_lay << is << "\t" << il << "\t" << ic << "\t";
+				if(il==6||il==5) tab_lay << "0.0000\t0.0000" << endl;
+				else tab_lay << par_lay[il-1][0] << "\t" << par_lay[il-1][1] << endl;
+			}
+		}
+	}
+	tab_pad.close();
+	tab_lay.close();
 
 	// -------------------------------------------------------------------------------------------------
 	// Deleting empty histograms
 	for(int i = 0 ; i < nHistos ; i++){
-		int notEmpty = (h2_T_vDt[i]->Integral());
+		int notEmpty = (h2_diff_adc_tdc[i]->Integral());
 		if(!notEmpty){
-			delete h2_T_vDt[i];
+			delete h2_diff_adc_tdc[i];
+		}
+
+		notEmpty = (h2_dMeantime_adc_layer[i]->Integral());
+		if(!notEmpty){
+			delete h2_dMeantime_adc_layer[i];
 		}
 	}
 
 	// -------------------------------------------------------------------------------------------------
 	// Saving plots to a pdf file
-	c0 -> Print("results_attenuation.pdf(");
-	c0 -> Print("results_adc_spectra.pdf(");
+	c0 -> Print("results_paddle_corr.pdf(");
+	cSLC_layer -> Print("results_paddle_corr.pdf");
+	c0 -> Print("results_paddle_corr.pdf");
+	cres -> Print("results_resolutions.pdf(");
 
 	for(int is = 0 ; is < 5 ; is++){
 		for(int il = 0 ; il < 5 ; il++){
-			cSLC[is][il] -> Print("results_attenuation.pdf");
-			cADC[is][il] -> Print("results_adc_spectra.pdf");
+			cSLC_paddle[is][il] -> Print("results_paddle_corr.pdf");
+			cSLC_resol [is][il] -> Print("results_resolutions.pdf");
 		}
 	}
-	c0 -> Print("results_attenuation.pdf)");
-	c0 -> Print("results_adc_spectra.pdf)");
-
+	c0 -> Print("results_paddle_corr.pdf)");
+	c0 -> Print("results_resolutions.pdf)");
+	*/
 	myapp -> Run();
 	return 0;
 }
@@ -334,30 +326,30 @@ void PrettyTH2F(TH2F * h2) {
 	h2 -> GetXaxis() -> SetTitleSize(0.09);
 	h2 -> GetYaxis() -> SetTitleSize(0.09);
 
-	h2 -> GetYaxis() -> SetTitleOffset(0.60);
+	h2 -> GetYaxis() -> SetTitleOffset(0.70);
 
 	h2 -> GetYaxis() -> SetNdivisions(109);
-	h2 -> GetXaxis() -> SetNdivisions(109);
+	h2 -> GetXaxis() -> SetNdivisions(107);
 }
 // ========================================================================================================================================
 void PrettyTH1F(TH1F * h1, int color) {
-        h1 -> GetXaxis() -> CenterTitle();
-        h1 -> GetYaxis() -> CenterTitle();
+	h1 -> GetXaxis() -> CenterTitle();
+	h1 -> GetYaxis() -> CenterTitle();
 
 	h1 -> SetLineColor(color);
 	h1 -> SetLineWidth(2);
 
-        h1 -> SetTitleSize(0.5);
+	h1 -> SetTitleSize(0.5);
 
-        h1 -> GetXaxis() -> SetLabelSize(0.09);
-        h1 -> GetYaxis() -> SetLabelSize(0.09);
-        h1 -> GetXaxis() -> SetTitleSize(0.09);
-        h1 -> GetYaxis() -> SetTitleSize(0.09);
+	h1 -> GetXaxis() -> SetLabelSize(0.09);
+	h1 -> GetYaxis() -> SetLabelSize(0.09);
+	h1 -> GetXaxis() -> SetTitleSize(0.09);
+	h1 -> GetYaxis() -> SetTitleSize(0.09);
 
-        h1 -> GetYaxis() -> SetTitleOffset(0.60);
+	h1 -> GetYaxis() -> SetTitleOffset(0.60);
 
-        h1 -> GetYaxis() -> SetNdivisions(109);
-        h1 -> GetXaxis() -> SetNdivisions(109);
+	h1 -> GetYaxis() -> SetNdivisions(109);
+	h1 -> GetXaxis() -> SetNdivisions(107);
 }
 // ========================================================================================================================================
 double getTriggerPhase( long timeStamp ) {
@@ -403,40 +395,17 @@ int getMaxNonEmptyBin(TH2F * h2){
 	return nBinY;
 }
 // ========================================================================================================================================
-void LoadEffectiveVelocity(){
-	ifstream f;
-	int layer, sector, component, barId;
-	double veff, temp;
-	f.open("input/effective_velocity.txt");
-	while(!f.eof()){
-		f >> layer;
-                f >> sector;
-                f >> component;
-                barId = 100*layer + 10*sector + component;
-		f >> veff;
-		f >> temp;
-		f >> temp;
-		f >> temp;
-		eff_c[barId] = veff;
-	}
-	f.close();
+void PrettyTGraphErrors(TGraphErrors * gP, int color){
+	gP -> SetTitle("");
+	gP -> SetMarkerColor(color);
+	gP -> SetMarkerStyle(20);
+	gP -> GetXaxis() -> CenterTitle();
+	gP -> GetXaxis() -> SetTitle("Bar ID");
+	gP -> GetYaxis() -> CenterTitle();
+        gP -> GetYaxis() -> SetTitle("resolution/#sqrt{2} [ps]");
+
+	gP -> GetYaxis() -> SetTitleOffset(1.30);
+
+	gP -> SetMinimum(150);
+	gP -> SetMaximum(400);
 }
-// ========================================================================================================================================
-void LoadEfrainsAttenuationLengths(){
-//mu_efrain[600] = {0};
-	ifstream f;
-	int layer, sector, component, barId;
-	double attL, temp;
-	f.open("input/attenuation_lengths.txt");
-	while(!f.eof()){
-		f >> layer;
-		f >> sector;
-		f >> component;
-		barId = 100*layer + 10*sector + component;
-		f >> attL;
-		f >> temp;
-		mu_efrain[barId] = attL;
-	}
-	f.close();
-}
-// ========================================================================================================================================
